cd26c4b72955e66190a9d06354070f94baf4e7d3
sagarjadhav180 <sagar.jadhav180@gmail.com>
2026-08-23 11:38:37 +0530

    test

--- src/test/java/com/framework/tests/contract/UserAdvancedContractTest.java
+++ src/test/java/com/framework/tests/contract/UserAdvancedContractTest.java
@@ -146,5 +146,6 @@ public class UserAdvancedContractTest extends BaseTest {
     public void placeholderMEthod(){
         System.out.println("harry branch");
         System.out.println("==THis is placeholder method1122===");
+        System.out.println("==main branch===");
     }
 }
